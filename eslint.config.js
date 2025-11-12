a190c301facd4416149520e4d7f801a23ad670ff
Amr Gad <merodiro@gmail.com>
2025-11-12 13:05:37 +0200

    update deps

--- eslint.config.js
+++ eslint.config.js
@@ -78,6 +78,7 @@ export default defineConfig(
         },
       ],
       'perfectionist/sort-imports': ['error'],
+      'perfectionist/sort-exports': ['error'],
       'unicorn/prevent-abbreviations': 'off',
       'unicorn/no-null': 'off',
       'unicorn/prefer-global-this': 'off',
