271363a873de3a0a9f429481d34f8bb2c56e0b4a
Amr Gad <merodiro@gmail.com>
2025-12-02 19:41:47 +0200

    update deps and use oxfmt for everything

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
-import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { betterAuth } from 'better-auth/minimal'
 import { username, organization } from 'better-auth/plugins'
 
 import { db } from '@/db'
